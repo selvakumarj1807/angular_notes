880a34ff10f5aa0f184e53d7c3cfa7ec9b4f1281
selva <selvakumarj1807@gmail.com>
2024-06-13 23:45:40 +0530

    2

--- my-first-project_02/src/app/shop/shop.component.ts
+++ my-first-project_02/src/app/shop/shop.component.ts
@@ -6,5 +6,49 @@ import { Component } from '@angular/core';
   styleUrl: './shop.component.css'
 })
 export class ShopComponent {
+  id: number = Math.floor(Math.random() * (10 - 1 + 1) + 1);
+  status: string = 'Opened';
 
+  constructor() {
+    this.status = Math.random() > 0.5 ? 'Opened' : 'Closed';
+  }
+
+  getColor(): string {
+    return this.status === 'Opened' ? 'yellow' : 'black';
+  }
 }
+
+
+
+
+/*
+1. Custom Data for ShopComponent:
+---------------------------------
+
+export class ShopComponent {
+  id: number = Math.floor(Math.random() * (10 - 1 + 1) + 1);
+  status: string = 'Opened';
+
+  constructor() {
+    this.status = Math.random() > 0.5 ? 'Opened' : 'Closed';
+  }
+}
+
+
+2. Attribute Directives:
+------------------------
+
+
+export class ShopComponent {
+  id: number = Math.floor(Math.random() * (10 - 1 + 1) + 1);
+  status: string = 'Opened';
+
+  constructor() {
+    this.status = Math.random() > 0.5 ? 'Opened' : 'Closed';
+  }
+
+  getColor(): string{
+    return this.status === 'Opened' ? 'yellow' : 'black';
+  }
+}
+*/
--- my-first-project_02/src/app/shopping-mall/shopping-mall.component.ts
+++ my-first-project_02/src/app/shopping-mall/shopping-mall.component.ts
@@ -24,154 +24,14 @@ export class ShoppingMallComponent {
 
   shopName: string = '';
 
-  shops: string[] = ['shop one', 'shop two'];
+  shops: string[] = ['shop one', 'shop two', 'shop three'];
 
 }
-
-
 
 
 /*
-
-  //Element Selector - mostly used Element Selector
-  //selector: 'app-shopping-mall',
-
-  //Attribute Selector
-  //selector: '[app-shopping-mall]',
-
-  //CSS Selector
-  //selector: '.app-shopping-mall',
-
-  
-1. String Interpolation - One way binding :
--------------------------------------------
-// imports - FormsModule in app.module.ts file is must
-
-export class ShoppingMallComponent {
-  shoppingMallName: string = 'Shopping Mall'; 
-}
-
-
-2. Property Binding - One way binding :
----------------------------------------
-        Dynamically access for button . use type 2. Property Binding. property binding for one element 
-
-export class ShoppingMallComponent {
-  shoppingMallName: string = 'Shopping Mall'; 
-  isButtonActive: boolean = true; 
-  
-  constructor() {
-    setTimeout(() => {
-      this.isButtonActive = false;
-    }, 4000)
-  }
-
-}
-
-
-3. Event Binding - One way binding :
-------------------------------------
-        Event Binding for trigger the button.
-
-Example 1:
-----------
-
-export class ShoppingMallComponent {
-  shoppingMallName: string = 'Shopping Mall'; //1. One way binding - String Interpolation
-  isButtonActive: boolean = true;   // Dynamically access for button . use 2. Property Binding
-  
-  constructor() {
-    setTimeout(() => {
-      this.isButtonActive = false;
-    }, 4000)
-  }
-
-  createNewShop: string = 'No Shop is added yet'; // 3. Event Binding for trigger the button.
-  onCreateShop(): void {
-    this.createNewShop = 'New Shop is Added ';
-  }
-
-}
-
-
-Example 2:
-----------
-
-export class ShoppingMallComponent {
-  shoppingMallName: string = 'Shopping Mall'; //1. One way binding - String Interpolation
-  isButtonActive: boolean = true;   // Dynamically access for button . use 2. Property Binding
-
-  constructor() {
-    setTimeout(() => {
-      this.isButtonActive = false;
-    }, 4000)
-  }
-
-  createNewShop: string = 'No Shop is added yet'; // 3. Event Binding for trigger the button.
-  onCreateShop(): void {
-    this.createNewShop = 'New Shop is Added ' + this.shopName;
-  }
- 
-    shopName: string = '';
-    onUpdateShopName(event: Event) {
-      this.shopName = (<HTMLInputElement>event.target).value;
-      console.log((<HTMLInputElement>event.target).value)
-    }
-  
-  }
-  
-
-4. Two-way-Binding :
---------------------
-Example 1:
-----------
-
-
-export class ShoppingMallComponent {
-  shoppingMallName: string = 'Shopping Mall'; //1. One way binding - String Interpolation
-  isButtonActive: boolean = true;   // Dynamically access for button . use 2. Property Binding
-
-  constructor() {
-    setTimeout(() => {
-      this.isButtonActive = false;
-    }, 4000)
-  }
-
-  createNewShop: string = 'No Shop is added yet'; // 3. Event Binding for trigger the button.
-  onCreateShop(): void {
-    this.createNewShop = 'New Shop is Added ' + this.shopName;
-  }
-
-  shopName: string = '';
-
-}
-
-
-Example 2:
-----------
-
-export class ShoppingMallComponent {
-  shoppingMallName: string = 'Shopping Mall'; //1. One way binding - String Interpolation
-  isButtonActive: boolean = true;   // Dynamically access for button . use 2. Property Binding
-
-  constructor() {
-    setTimeout(() => {
-      this.isButtonActive = false;
-    }, 4000)
-  }
-
-  createNewShop: string = 'No Shop is added yet'; // 3. Event Binding for trigger the button.
-  onCreateShop(): void {
-    this.createNewShop = 'New Shop is Added ' + this.shopName;
-  }
-
-  shopName: string = 'Shop Name';
-
-}
-
-
-Example 3:
-----------
+First:
+-----
 
 export class ShoppingMallComponent {
   shoppingMallName: string = 'Shopping Mall'; //1. One way binding - String Interpolation
@@ -192,17 +52,13 @@ export class ShoppingMallComponent {
 
   shopName: string = '';
 
+  shops: string[] = ['shop one', 'shop two', 'shop three'];
+
 }
 
 
-Example 4:
-----------
-      no change
-
-
-Example 5:
-----------
-
+3. push the shopName into the shopping-mall.component.ts file:
+--------------------------------------------------------------
 
 export class ShoppingMallComponent {
   shoppingMallName: string = 'Shopping Mall'; //1. One way binding - String Interpolation
@@ -219,11 +75,12 @@ export class ShoppingMallComponent {
   onCreateShop(): void {
     this.createNewShop = 'New Shop is Added ' + this.shopName;
     this.isShopCreated = true;
+    this.shops.push(this.shopName);
   }
 
   shopName: string = '';
 
-  shops: string[] = ['shop one', 'shop two'];
+  shops: string[] = ['shop one', 'shop two', 'shop three'];
 
 }
 
