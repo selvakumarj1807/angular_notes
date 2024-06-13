31963de127a6c10a689f85c7fdf286b5acf228f4
selva <selvakumarj1807@gmail.com>
2024-06-13 22:54:26 +0530

    2

--- my-first-project_02/src/app/shopping-mall/shopping-mall.component.ts
+++ my-first-project_02/src/app/shopping-mall/shopping-mall.component.ts
@@ -6,8 +6,28 @@ import { Component } from '@angular/core';
   styleUrl: './shopping-mall.component.css'
 })
 export class ShoppingMallComponent {
-  shoppingMallName: string = 'Shopping Mall'; 
+  shoppingMallName: string = 'Shopping Mall'; //1. One way binding - String Interpolation
+  isButtonActive: boolean = true;   // Dynamically access for button . use 2. Property Binding
+
+  constructor() {
+    setTimeout(() => {
+      this.isButtonActive = false;
+    }, 4000)
+  }
+
+  createNewShop: string = 'No Shop is added yet'; // 3. Event Binding for trigger the button.
+  isShopCreated: boolean = false;
+  onCreateShop(): void {
+    this.createNewShop = 'New Shop is Added ' + this.shopName;
+    this.isShopCreated = true;
+  }
+
+  shopName: string = '';
+
+  shops: string[] = ['shop one', 'shop two'];
+
 }
+
 
 
 
