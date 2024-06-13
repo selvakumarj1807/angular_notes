cf8741aa1f4cd684721f188b5d9b4f18c9e84a89
selva <selvakumarj1807@gmail.com>
2024-06-13 17:42:37 +0530

    first commit

---
+++ my-first-project_01/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ShoppingMallComponent } from './shopping-mall/shopping-mall.component';
+import { FormsModule } from '@angular/forms';
+import { ShopComponent } from './shop/shop.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ShoppingMallComponent,
+    ShopComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
