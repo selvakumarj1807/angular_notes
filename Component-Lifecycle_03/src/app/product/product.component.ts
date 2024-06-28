51b3bd8a5b4308b9f0f54bd6c9b2ce6b6d25598a
selva <selvakumarj1807@gmail.com>
2024-06-28 06:47:43 +0530

    4

--- Component-Lifecycle_03/src/app/product/product.component.ts
+++ Component-Lifecycle_03/src/app/product/product.component.ts
@@ -8,6 +8,43 @@ import { IProduct } from '../_models/product.model';
 })
 export class ProductComponent {
   @Input() product: IProduct;
+
+  constructor() {
+    console.log('1. Constructor Called');
+  }
+
+  ngOnInit() {
+    console.log('3. ngOnInit Called');
+  }
+
+  ngOnChanges() {
+    console.log('2. ngOnChanges Called');
+  }
+
+  ngDoCheck() {
+    console.log('4. ngDoCheck Called');
+  }
+
+  ngAfterContentInit() {
+    console.log('5. ngAfterContentInit run only once for load ng-content');
+  }
+
+  ngAfterContentChecked() {
+    console.log('6. ngAfterContentChecked called');
+  }
+
+
+  ngAfterViewInit() {
+    console.log('7. ngAfterViewInit Called');
+  }
+
+  ngAfterViewChecked() {
+    console.log('8. ngAfterViewChecked called')
+  }
+
+  ngDestroy() {
+    console.log('9. ngDestroy Called only if delete some product or use delete method')
+  }
 }
 
 /*
@@ -22,6 +59,51 @@ Example 2:
 
 export class ProductComponent {
   @Input() product: IProduct;
+}
+
+
+Example 3:
+----------
+
+export class ProductComponent {
+  @Input() product: IProduct;
+
+  constructor() {
+    console.log('1. Constructor Called');
+  }
+
+  ngOnInit() {
+    console.log('3. ngOnInit Called');
+  }
+
+  ngOnChanges() {
+    console.log('2. ngOnChanges Called');
+  }
+
+  ngDoCheck() {
+    console.log('4. ngDoCheck Called');
+  }
+
+  ngAfterContentInit(){
+    console.log('5. ngAfterContentInit run only once for load ng-content');
+  }
+
+  ngAfterContentChecked(){
+    console.log('6. ngAfterContentChecked called');
+  }
+
+
+  ngAfterViewInit(){
+    console.log('7. ngAfterViewInit Called');
+  }
+
+  ngAfterViewChecked(){
+    console.log('8. ngAfterViewChecked called')
+  }
+
+  ngDestroy(){
+    console.log('9. ngDestroy Called only if delete some product or use delete method')
+  }
 }
 
 */
