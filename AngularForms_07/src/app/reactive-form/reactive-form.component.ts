c66aee236a4970da7da92cf034999f9b1f33bab4
selva <selvakumarj1807@gmail.com>
2024-07-10 07:24:30 +0530

    reactive forms

--- AngularForms_07/src/app/reactive-form/reactive-form.component.ts
+++ AngularForms_07/src/app/reactive-form/reactive-form.component.ts
@@ -8,6 +8,33 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class ReactiveFormComponent {
   contactForm = new FormGroup({
+    firstName: new FormControl('selva'),
+    lastName: new FormControl({
+      value: 'kumar',
+      disabled: true // disabled: false
+    }),
+    email: new FormControl(),
+    gender: new FormControl(),
+    isMarried: new FormControl(),
+    country: new FormControl(),
+    address: new FormGroup({
+      city: new FormControl(),
+      pincode: new FormControl()
+    })
+  });
+
+  onSubmit() {
+    console.log(this.contactForm.value);
+  }
+}
+
+
+/*
+Example 1:
+----------
+
+export class ReactiveFormComponent {
+  contactForm = new FormGroup({
     firstName: new FormControl(),
     lastName: new FormControl(),
     email: new FormControl(),
@@ -20,3 +47,27 @@ export class ReactiveFormComponent {
     console.log(this.contactForm.value);
   }
 }
+
+Example 2:
+----------
+
+export class ReactiveFormComponent {
+  contactForm = new FormGroup({
+    firstName: new FormControl(),
+    lastName: new FormControl(),
+    email: new FormControl(),
+    gender: new FormControl(),
+    isMarried: new FormControl(),
+    country: new FormControl(),
+    address: new FormGroup({
+      city: new FormControl(),
+      pincode: new FormControl()
+    })
+  });
+
+  onSubmit() {
+    console.log(this.contactForm.value);
+  }
+}
+
+*/
