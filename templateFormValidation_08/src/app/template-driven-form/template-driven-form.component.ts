a9efb9272f5ed09516a8892cc694ba2c1953efb7
selva <selvakumarj1807@gmail.com>
2024-07-16 15:51:53 +0530

    2

--- templateFormValidation_08/src/app/template-driven-form/template-driven-form.component.ts
+++ templateFormValidation_08/src/app/template-driven-form/template-driven-form.component.ts
@@ -103,4 +103,49 @@ class Cantact {
 
 Example 2:
 ----------
+
+export class TemplateDrivenFormComponent implements OnInit {
+  countryList: Country[] = [
+    new Country('1', 'India'),
+    new Country('2', 'USA'),
+    new Country('3', 'England'),
+  ];
+
+  onSubmit(form: NgForm) {
+    console.log('Form Submitted!', form.value); //if check the form element -  console.log('Form Submitted!', form);
+  }
+
+
+  contact!: Cantact; // ! - later Value initializer
+
+  ngOnInit(): void {
+    this.contact = {
+      firstName: "",
+      lastName: "Technology",
+      email: "tit@gmail.com",
+      gender: "male",
+      isMarried: true,
+      country: "2",
+      address: { city: "Dindigul", pincode: "624001" }
+    }
+  }
+
+}
+
+class Country {
+  constructor(public id: string, public name: string) { }
+}
+
+class Cantact {
+  firstName!: string;
+  lastName!: string;
+  email!: string;
+  gender!: string;
+  isMarried!: boolean;
+  country!: string;
+  address!: {
+    city: string;
+    pincode: string;
+  }
+}
 */
